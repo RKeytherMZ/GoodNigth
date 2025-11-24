366c97f7b0b83ea12a403f5cf73909aa026cc2b3
RKeytherMZ <rubbykeyther@gmail.com>
2025-11-23 21:58:31 -0400

    add database configuration and initial migration
    Added EF Core configuration and initial database migration.

---
+++ DremasSystem/Models/Dream.cs
@@ -0,0 +1,11 @@
+﻿namespace DremasSystem.Models
+{
+    public class Dream
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime Fecha { get; set; }
+        public int Intensidad { get; set; }
+    }
+}
